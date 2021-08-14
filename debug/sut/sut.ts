90066f21d2ef8ce8ff9e805f7cdb9f4dfd8cf1df
Josh Kraker <joshkrak@gmail.com>
2021-08-13 20:41:23 -0400

    Readme

--- debug/sut/sut.ts
+++ debug/sut/sut.ts
@@ -2,7 +2,15 @@
 import { jsonSchema } from '../../src';
 
 export interface User {
-    readonly id: [string, ...string[]];
+    readonly id: string;
+    name: string;
+    description?: string | null;
+    items: UserItem[];
 }
 
-const _ = jsonSchema<User>();
+export interface UserItem {
+    foo: string;
+    bar: string | number;
+}
+
+const test = jsonSchema<User>();
